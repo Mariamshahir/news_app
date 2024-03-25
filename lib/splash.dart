a636cf3a7378cdefdcf0e7134bb4535ad461e747
Mariam Shahir <mariamshahir2000@gmail.com>
2024-03-25 12:22:29 +0200

    Finished splash screen.

---
+++ lib/splash.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:news_app/homescreen.dart';
+import 'package:news_app/utils/assets_app.dart';
+
+class Splash extends StatefulWidget {
+  static const String routeName = "splash";
+
+  const Splash({super.key});
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+            image: AssetImage(AppAssets.splash), fit: BoxFit.fill),
+      ),
+      child: const Scaffold(
+        backgroundColor: Colors.transparent,
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.of(context).pushReplacement(MaterialPageRoute(
+        builder: (_) => const HomeScreen(),
+      ));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+        overlays: SystemUiOverlay.values);
+  }
+}
