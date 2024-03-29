5ddb08f22abae704e9dfeef9b609a0ff07bdff21
Mariam Shahir <mariamshahir2000@gmail.com>
2024-03-29 15:13:33 +0200

    Finished localizations and shared preferences.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:news_app/homescreen.dart';
+import 'package:news_app/provider/language_provider.dart';
 import 'package:news_app/splash.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  LanguageProvider languageProvider = LanguageProvider();
+  await languageProvider.setItems();
+
+  runApp(
+    MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => languageProvider),
+    ], child: const MyApp()),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -11,8 +25,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    LanguageProvider provider = Provider.of(context);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: const [Locale("en"), Locale("ar")],
+      locale: Locale(provider.currentLocale),
       routes: {
         Splash.routeName: (_) => Splash(),
         HomeScreen.routeName: (_) => HomeScreen(),
